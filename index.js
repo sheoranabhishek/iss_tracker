dcf985dc891d17d45f5b9274fa1ed09eeb3049c3
sheoranabhishek <abhisheksheoran2000@gmail.com>
2020-04-19 23:09:49 +0530

    Live Updating

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
+// Making a map and tiles
+const mymap = L.map("satmap").setView([0, 0], 1);
+const attribution =
+  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
+const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+const tiles = L.tileLayer(tileUrl, { attribution });
+tiles.addTo(mymap);
+
+// Making a marker with a custom icon
+const issIcon = L.icon({
+  iconUrl:
+    "https://raw.githubusercontent.com/CodingTrain/Intro-to-Data-APIs-JS/source/module1/03_fetch_json/iss200.png",
+  iconSize: [50, 32],
+  iconAnchor: [25, 16],
+});
+const marker = L.marker([0, 0], { icon: issIcon }).addTo(mymap);
+
 const api_url = "https://api.wheretheiss.at/v1/satellites/25544";
 
-getposition();
+let firstTime = true;
 
-async function createMap(lat, lon) {
-  var satIcon = L.icon({
-    iconUrl:
-      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F2%2F27%2FFP_Satellite_icon.svg%2F1024px-FP_Satellite_icon.svg.png&f=1&nofb=1",
-    iconSize: [50, 50],
-  });
-  const mymap = L.map("satmap").setView([lat, lon], 5);
-  const attribution =
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
-  const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
-  const tiles = L.tileLayer(tileUrl, { attribution });
-  tiles.addTo(mymap);
-  L.marker([lat, lon], { icon: satIcon, watch: true, setView: true }).addTo(
-    mymap
-  );
-}
-
-async function getposition() {
+async function getISS() {
   const response = await fetch(api_url);
   const data = await response.json();
   const { latitude, longitude } = data;
+
+  marker.setLatLng([latitude, longitude]);
+  if (firstTime) {
+    mymap.setView([latitude, longitude], 2);
+    firstTime = false;
+  }
   console.log(data);
-  document.getElementById("lat").textContent = latitude;
-  document.getElementById("lon").textContent = longitude;
-  createMap(latitude, longitude);
+  document.getElementById("lat").textContent = latitude.toFixed(2);
+  document.getElementById("lon").textContent = longitude.toFixed(2);
 }
+
+getISS();
+
+setInterval(getISS, 1000);
